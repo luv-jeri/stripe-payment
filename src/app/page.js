89f11e19cb7ca0eefeb9cfaaefea27a29f52cc4d
Sanjay Kumar Gautam <luvvjeri@gmail.com>
2023-07-09 20:14:25 +0530

    Added Title

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
@@ -10,14 +10,14 @@ import Stripe from "stripe";
 //   "sk_test_51NOWyHSDQH8ONy11WZQP3PY5FJI0eFzt4UpfIASrKN1tjBS9iaNGqgX7ncIIxQWJSmxJ4kM0X9LckZvvjztkCV4X00WGhM8w7l"
 // )
 
-  // const createPaymentIntent = async (cb) => {
-  //   const paymentIntent = await stripe.paymentIntents.create({
-  //     currency: "EUR",
-  //     amount: 1999,
-  //     automatic_payment_methods: { enabled: true },
-  //   });
-  //   cb(paymentIntent.client_secret);
-  // }
+// const createPaymentIntent = async (cb) => {
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     currency: "EUR",
+//     amount: 1999,
+//     automatic_payment_methods: { enabled: true },
+//   });
+//   cb(paymentIntent.client_secret);
+// }
 
 export default function Page() {
   const [stripePromise, setStripePromise] = useState(null);
@@ -37,32 +37,44 @@ export default function Page() {
       body: JSON.stringify({
         // send the amount to the client for the next step
       }),
-    }).then(async (result) => {
-      var { clientSecret } = await result.json();
-      setClientSecret(clientSecret);
-      setLoading(false);
-    }).catch((err) => {
-      console.log("err", err)
-    });
-    
+    })
+      .then(async (result) => {
+        var { clientSecret } = await result.json();
+        setClientSecret(clientSecret);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
   }, []);
-  
-  if(loading) {
-    return <h2>Loading...</h2>
+
+  if (loading) {
+    return <h2>Loading...</h2>;
   }
 
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-    }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Next.js v13.4 + Stripe Checkout
+      </h1>
       {clientSecret && stripePromise ? (
         <Elements stripe={stripePromise} options={{ clientSecret }}>
           <CheckoutForm />
         </Elements>
-      ) : <h2>Error...</h2>}
+      ) : (
+        <h2>Error...</h2>
+      )}
     </div>
   );
 }
